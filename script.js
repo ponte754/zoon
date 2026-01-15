65c342f182102807b2c5a16ce4b1b9abc5c0d562
ponte754 <yazansaher55@gmail.com>
2026-01-15 15:11:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+//navbar//
 function toggleMenu(){
     document.getElementById("nav-link").classList.toggle("show");
 }
+//end navbar//
 
+//popup//
 function openPopup(prouct){
     document.getElementById("popup").style.display = "block";
 
@@ -18,3 +21,20 @@ function popup(productName){
  function closePoupup(){
         document.getElementById("popup").style.display = "none";
     } 
+
+    //end popup//
+
+    // Forms//
+document.getElementById("FORMS").onsubmit = function(e) {
+  e.preventDefault();
+
+  fetch("https://formspree.io/f/xnjnewzr", {
+    method: "POST",
+    body: new FormData(this),
+    headers: { "Accept": "application/json" }
+  }).then(() => {
+    window.location.href = "Thanks.html";
+  });
+};
+
+//end forms//
